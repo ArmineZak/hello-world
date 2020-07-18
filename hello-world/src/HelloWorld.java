1c738efce502421ed98b294f7017ef879acd34f6
ArmineZak <armzak91@gmail.com>
2020-07-18 17:10:32 +0400

    -added !!!!

--- hello-world/src/HelloWorld.java
+++ hello-world/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
      * The main method is searched by JVM as a starting point for the app
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");//the cursor remains at the start of the next line
+        System.out.println("Hello world!!!!");//the cursor remains at the start of the next line
        /*
         System.out.println("Actually, my first project was going to be...Houston, I've got a problem");
         System.out.println("...or more correctly...Houston, I am the problem.");
